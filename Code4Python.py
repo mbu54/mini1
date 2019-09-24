9adc79f3a0ccc99251fc6dc01e6dbf97a8f2a0d9
AtlasWare123 <55196816+AtlasWare123@users.noreply.github.com>
2019-09-23 20:42:21 -0400

    edit the time function
    now the time function can output time correctly

--- Code4Python.py
+++ Code4Python.py
@@ -4,10 +4,10 @@ import tweepy as tw
 import pandas as pd
 
 #Twitter API credentials
-consumer_key = "..."
-consumer_secret = "..."
-access_key = "..."
-access_secret = "..."
+consumer_key = "epSN7DLvzimhRPbFikys8n1Vb"
+consumer_secret = "r6MnsK1VLPnSopb9RUakdKvqHGW0WfVbqqiicfX6ZofJrJvzlO"
+access_key = "1171550663857332226-VOPUcYhjRmVD9KnaNp8PUtC8ABMWkw"
+access_secret = "pQVMIyzT354TbGfwH99g1f0j2K29ZpesLZZ4wXYdGvQtG"
 
 def get_tweet(username):
   twitter_feed = []
@@ -22,27 +22,28 @@ def get_tweet(username):
 
 #print(twitter_ano)
 
-def hashtags(search_words,date_since):
+def hashtags(search_words,date_since,date_until):
 # Authorize 
   twitter_emp = []
   
   auth = tw.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_key,access_secret)
   api = tw.API(auth, wait_on_rate_limit=True)
-  tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since).items(50)
+  tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since,until=date_until).items(100)
   # Iterate and print tweets
   for tweet_tag in tweets:
     #twitter_ctime=twitter_emp+[tweet_tag.created_at]
+    #print(tweet_tag.text)
     twitter_emp=twitter_emp+[tweet_tag.text]
   return twitter_emp
   
-def hashtags_ctime(search_words,date_since):
+def hashtags_ctime(search_words,date_since,date_until):
 # Authorize 
   twitter_ctime=[]
   auth = tw.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_key,access_secret)
   api = tw.API(auth, wait_on_rate_limit=True)
-  tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since).items(50)
+  tweets = tw.Cursor(api.search,q=search_words,lang="en",since=date_since,until=date_until).items(300)
   # Iterate and print tweets
   for tweet_tag in tweets:
     #twitter_ctime=twitter_emp+[tweet_tag.created_at]
@@ -51,13 +52,15 @@ def hashtags_ctime(search_words,date_since):
 
 #Content for sb Timeline
 twitter_results=get_tweet("@realDonaldTrump") 
+#print(type(twitter_results))
 
 #Content for Harhstags
 wanted_words = "#wildfires"
-wangted_date_since = "2018-11-16"
-twiter_tag_content=hashtags(wanted_words,wangted_date_since)
-twitter_created_time=hashtags_ctime(wanted_words,wangted_date_since)
-print(twitter_created_time)
+wangted_date_since = "2019-09-17"
+wangted_date_until = "2019-09-20"
+twiter_tag_content=hashtags(wanted_words,wangted_date_since,wangted_date_until)
+twitter_created_time=hashtags_ctime(wanted_words,wangted_date_since,wangted_date_until)
+#print(twitter_created_time)
 #strip http for sb  
 twitter_ano1=[]
 for s in twitter_results:
@@ -71,17 +74,19 @@ twitter_ano2=[]
 for s in twiter_tag_content:
   if s.find('https')>0:
     s=s[0:s.find('https')-1]
-    #print(s)
+   # print(s)
     twitter_ano2.append(s)
 #print(twitter_ano2)
 
 #strip datetime.datetime for hashtags
 twitter_ano3=[]
 for s in twitter_created_time:
-  print (type(s))
+  print (s)
+  twitter_ano3.append(s)
   #if s.find(' datetime.datetime')>0:
    # s=s[0:s.find(' datetime.datetime')-1]
     #print(s)
     #twitter_ano3.append(s)
+#print(type(twitter_ano3))
 
 #print(twitter_ano3)
